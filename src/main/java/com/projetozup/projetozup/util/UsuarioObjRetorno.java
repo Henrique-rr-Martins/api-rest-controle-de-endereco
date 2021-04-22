9808770a8d8f3c242e6961f0fd10cb412675c91b
Henrique Martins <henrique_rr_martins@hotmail.com>
2021-04-22 00:02:48 -0300

    Ajuste objeto de retorno

--- src/main/java/com/projetozup/projetozup/util/UsuarioObjRetorno.java
+++ src/main/java/com/projetozup/projetozup/util/UsuarioObjRetorno.java
@@ -14,5 +14,5 @@ import lombok.NoArgsConstructor;
 public class UsuarioObjRetorno {
 	private int cdRetorno;
 	private String msgRetorno;
-	private Usuario listaUsuario;
+	private Usuario Usuario;
 }
