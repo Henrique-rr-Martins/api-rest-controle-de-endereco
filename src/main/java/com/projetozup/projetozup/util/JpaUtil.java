4f1c025f01923d3db8f781ca313da86063a6d406
Henrique Martins <henrique_rr_martins@hotmail.com>
2021-04-23 09:15:48 -0300

    Ajuste classe JpaUtil para somente testar conexão direta via JPA;

--- src/main/java/com/projetozup/projetozup/util/JpaUtil.java
+++ src/main/java/com/projetozup/projetozup/util/JpaUtil.java
@@ -4,18 +4,21 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * Classe para teste de conexão com o banco. Assim é  possível verificar  se o problema é na abstração de alguma dependência ou na conexão em si.
+ * 
+ * @author Henrique Martins
+ *
+ */
 public class JpaUtil {
-	private static EntityManagerFactory factory;
 	
-	static {
-		factory = Persistence.createEntityManagerFactory("ZUP");
-	}
-	
-	public static EntityManager getEntityManager() {
-		return factory.createEntityManager();
-	}
-	
-	public static void close() {
-		factory.close();
+	/**
+	 * Checa se a aplicação se conecta ao banco.
+	 */
+	public static void testaConexao() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("ZUP");
+		EntityManager em = factory.createEntityManager();
+		System.out.println(em.isOpen());
+		em.close();
 	}
 }
